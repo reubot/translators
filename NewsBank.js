4bfe377ee9ed6b36a32840342d8235f43a9441f6
Reuben <reuben_p@yahoo.com>
2021-08-07 16:02:59 +1000

    add license block

--- NewsBank.js
+++ NewsBank.js
@@ -12,6 +12,29 @@
 	"lastUpdated": "2021-08-07 04:32:01"
 }
 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	NewsBank translator Copyright © 2021 Reuben Peterkin
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
 function detectWeb(doc, url) {
 	if (getRISText(doc)) return "newspaperArticle";
 	if (url.includes("results")) return "multiple";
